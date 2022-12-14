b092495b1f29728f1500a4f3431216e0e8f0988f
ATRandom <109669420+ATRandom-code@users.noreply.github.com>
2022-12-13 20:48:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-# The function takes in the secret word and the letters
-# that the player has guessed so far as inputs
-# and returns the current state of the game as a string
-def hangman(secret_word: str, letters_guessed: List[str]):
-  # Create a string that will be used to store the current state of the game
-  current_state = ""
-  
-  # Loop over each character in the secret word
-  for char in secret_word:
-    # If the character is in the list of letters guessed,
-    # add it to the current state string
-    if char in letters_guessed:
-      current_state += char
-    # Otherwise, add an underscore to the string to
-    # represent a letter that has not been guessed yet
-    else:
-      current_state += "_"
-  
-  # Return the current state of the game
-  return current_state
+import random
 
-# Run the function with a secret word and a list of letters guessed
-# as inputs to see the current state of the game
-print(hangman("python", ["p", "t", "h"]))
+# The list of words to choose from.
+words = ["apple", "banana", "orange", "grape", "strawberry"]
+
+# Generate a random word from the list.
+secret_word = random.choice(words)
+
+# Set the number of tries the player has.
+tries = 5
+
+# Start the game.
+while tries > 0:
+    # Get the player's guess.
+    guess = input("Enter your guess: ")
+
+    # If the guess is correct, the player wins.
+    if guess == secret_word:
+        print("You win!")
+        break
+
+    # If the guess is incorrect, subtract one from the number of tries.
+    else:
+        tries -= 1
+        print("Incorrect. You have %d tries left." % tries)
+
+# If the player runs out of tries, the player loses.
+if tries == 0:
+    print("You lose!")
